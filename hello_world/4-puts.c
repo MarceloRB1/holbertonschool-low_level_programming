4af786971455e270ded5bc4b1287ef6ba8d1b3d4
Marcelo <marcelorodb@gmail.com>
2022-06-06 09:57:35 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,13 @@
 # include <stdio.h>
  /**
-   * main - prints the phrase
+   * main - Entry point
    *
-   * Return: 0 if exited properly, non-zero otherwise
+   * Return: Always 0
  */
 int main(void)
 {
 
 
 		puts("\"Programming is like building a multilingual puzzle");
-		
 		return (0);
 }
