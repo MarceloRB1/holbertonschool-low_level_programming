bad7c9aff62dca136b58b49dbaa340c95b5900ad
Marcelo <marcelorodb@gmail.com>
2022-06-08 16:32:03 -0700

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
- *   */
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
 void print_alphabet(void)
 {
 		char ch;
 
 			ch = 'a';
 
-				while (ch <= 'z')
+while (ch <= 'z')
 						{
 									_putchar(ch);
 											ch++;
