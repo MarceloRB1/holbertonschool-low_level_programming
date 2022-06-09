2f19f1e70a1e6ad7a7193efbf8421fa67f7916ef
Marcelo <marcelorodb@gmail.com>
2022-06-09 08:54:22 -0700

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -4,10 +4,10 @@
  *  * _abs - computes the absolute value of an integer
  *   * @n: the int to check
  *    * Return: the absolute value of int
- *     */
+*/
 int _abs(int n)
 {
-		if (n >= 0)
+if (n >= 0)
 				{
 							return (n);
 								}
