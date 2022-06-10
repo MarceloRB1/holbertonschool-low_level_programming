9ac8562285ecb2a67f7dd8857751533c2cf7a23b
Marcelo <marcelorodb@gmail.com>
2022-06-10 06:16:00 -0700

    10000

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -5,12 +5,10 @@
  *   * @a: int to be added to b
  *    * @b: int to be added to a
  *     * Return: sum of a and b
- *      */
+*/
 int add(int a, int b)
 {
-		int c;
-
-			c = a + b;
-
-				return (c);
+int c;
+	c = a + b;
+return (c);
 }
