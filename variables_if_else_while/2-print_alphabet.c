6f185bf3e60ee483f54ea4af1d2a5277c454605d
Marcelo <marcelorodb@gmail.com>
2022-06-10 14:35:21 -0700

    3be

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,15 +4,14 @@
  *  * main - prints the alphabet in lowercase,
  *   * followed by a new line
  *    * Return: Always 0 (Success)
- *     */
+*/
 int main(void)
 {
-		char ch;
-
-			for (ch = 'a'; ch <= 'z'; ch++)
-					{
-								putchar(ch);
-									}
-				putchar('\n');
-					return (0);
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
