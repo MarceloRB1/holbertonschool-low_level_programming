4bc7317d7e01573a93f2c90c87eaae8b391ed980
Marcelo <marcelorodb@gmail.com>
2022-06-11 05:55:49 -0700

    1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "main.h"
-/** 
- * _isdigit - checks for a digit 
- * @c digit to be checked
- * Return: 1 if c is a digit, 0 otherwise
+/**
+ *	_isdigit - checks for a digit
+ *	@c: digit to be checked
+ *	Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
