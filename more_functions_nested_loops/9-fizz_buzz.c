4eb472551f3e4e9eca62a9ea91d05775d2137741
Marcelo <marcelorodb@gmail.com>
2022-06-11 09:58:36 -0700

    fizz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints number from 1 to 100, except when it is multiple of 3 and 5
+ * and wich cases it should print fizz and buzz respctively
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 != 0)
+			printf("Fizz");
+	} else if (a % 5 == 0 && a % 3 != 0)
+	{
+		printf("Buzz");
+	} else if (a % 3 == 0 && a % 5 == 0)
+	{
+		printf("FizzBuzz");
+	} else if (i == 1)
+	{
+		printf("%d", i);
+	} else
+	{
+		printf(" %d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
