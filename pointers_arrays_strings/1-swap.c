652875d1f833b09ab2376d3da668c50a478aec52
Marcelo <marcelorodb@gmail.com>
2022-06-14 13:44:12 -0700

    task1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,7 +3,7 @@
 /**
  * swap_int - swaps the values of two integers
  * @a: pointer ti first value
- * @b; pointer to second value
+ * @b: pointer to second value
  */
 
 void swap_int(int *a, int *b)
