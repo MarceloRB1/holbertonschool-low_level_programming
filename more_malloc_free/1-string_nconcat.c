4ab7eed1d46ff924c1050f248c8d62375f131e0f
Marcelo <marcelorodb@gmail.com>
2022-06-26 14:59:12 -0700

    111

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -24,17 +24,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (!s)
-	return (NULL);
+		return (NULL);
 
 	while (i < len1)
 	{
 		s[i] = s1[i];
-			i++;
+		i++;
 	}
 	while (n < len2 && i < (len1 + n))
 		s[i++] = s2[j++];
+
 	while (n >= len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
+
 	s[i] = '\0';
 
 	return (s);
