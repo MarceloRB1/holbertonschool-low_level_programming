eab8c4a5b56520cebe34e6b80b6a04ee1ee3a0d8
Marcelo <marcelorodb@gmail.com>
2022-07-13 07:18:44 -0700

    task0.8

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,28 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
- *
- * Return: the number of nodes printed
+ * print_list - prints all the elements of a list_t list
+ * @h: struct list_t
+ * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t res = 0;
 
 	while (h)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
+		if (h->str)
 			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 		h = h->next;
-		s++;
+		res++;
 	}
-
-	return (s);
+	return (res);
 }
