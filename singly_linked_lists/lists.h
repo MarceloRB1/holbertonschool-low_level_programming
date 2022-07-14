0743087c9f5d8187809ad711add4959d8287a55e
Marcelo <marcelorodb@gmail.com>
2022-07-14 16:37:52 -0700

    34

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,22 +1,25 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
+ * struct list - singly linked list
+ * @str: string of char
+ * @len: integer
+ * @next: pointer to the next node
  */
-typedef struct list_s
+
+typedef struct list
 {
 	char *str;
 	unsigned int len;
-	struct list_s *next;
+	struct list *next;
 } list_t;
 
+int _strlen(const char *s);
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
